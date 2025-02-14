c8ece6a5884d3cc8a9888967983c7290f0520fd2
l15121400@morelia.tecnm.mx <lalomonroy@192.168.1.8>
2025-02-13 20:52:08 -0600

    mi carta v3

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-const envoltura = document.querySelector(".envoltura-sobre");
-const carta = document.querySelector(".carta");
-
-document.addEventListener("click", (e) => {
-    if (e.target.matches(".sobre") || 
-        e.target.matches(".solapa-derecha") ||
-        e.target.matches(".solapa-izquierda") ||
-        e.target.matches(".corazon")) {
-        envoltura.classList.toggle("abierto");
-      
-    } else if (e.target.matches(".sobre *")) {
-        if (!carta.classList.contains("abierta")) {
-            carta.classList.add("mostrar-carta");
-
-            setTimeout(() => {
-                carta.classList.remove("mostrar-carta");
-                carta.classList.add("abierta");
-            }, 500);
-            envoltura.classList.add("desactivar-sobre")
-        } else {
-            carta.classList.add("cerrando-carta");
-            envoltura.classList.remove("desactivar-sobre");
-
-            setTimeout(() => {
-                carta.classList.remove("cerrando-carta")
-                carta.classList.remove("abierta")
-            }, 500);
-        }
-
-    } 
-})
+$(document).ready(function(){
+    // Al cargar la página, ocultamos las cortinas
+    $('.left-curtain').css('width', '0%');
+    $('.right-curtain').css('width', '0%');
+  
+    $('.valentines-day').click(function(){
+      // Animación de desvanecimiento de los elementos del sobre
+      $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+      $('.envelope').fadeOut(800, function() {
+        // Ocultar elementos dentro de .valentines-day
+        $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+        
+  
+        // Hacer visible la carta con una animación ondulante
+        $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+        $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+          var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+          $(this).css('transform', 'scale(' + scale + ')');
+        }}); // Animación de ondulación
+      });
+    });
+  });
